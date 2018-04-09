5ff68b5a0613fc1221afec3dbecaa863904f8bd7
Zachary Primus <zprimus@mail.usf.edu>
2018-04-08 20:50:36 -0400

    Updated

--- i2c.c
+++ i2c.c
@@ -1,30 +1,8 @@
-#include <xc.h>
 #include "i2c.h"
 
-//PIC12F1822 Configuration Bit Settings
-
-#define _XTAL_FREQ 4000000  // Fosc
-#define I2C_SLAVE 0x0A  // address without R/W bit, bit shifted left to make 14h or 15h
-
-// CONFIG1
-#pragma config FOSC = INTOSC    // Oscillator Selection (INTOSC oscillator: I/O function on CLKIN pin)
-#pragma config WDTE = OFF       // Watchdog Timer Enable (WDT disabled)
-#pragma config PWRTE = OFF      // Power-up Timer Enable (PWRT disabled)
-#pragma config MCLRE = OFF      // MCLR Pin Function Select (MCLR/VPP pin function is digital input)
-#pragma config CP = ON          // Flash Program Memory Code Protection (Program memory code protection is enabled)
-#pragma config BOREN = OFF      // Brown-out Reset Enable (Brown-out Reset enabled)
-#pragma config CLKOUTEN = OFF   // Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
-
-// CONFIG2
-#pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
-#pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
-#pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
-//#pragma config LPBOR = OFF      // Low-Power Brown Out Reset (Low-Power BOR is disabled)
-#pragma config LVP = ON         // Low-Voltage Programming Enable (Low-voltage programming enabled)
 
 // Initialize Reading Sequence from OMRON 44L-D6T
-void i2c_read_command(unsigned char address)
-{
+void i2c_read_command() {
     i2c_Start();                        // send Start bit
     i2c_Address(I2C_SLAVE, I2C_WRITE);  // Write to slave address
     i2c_Write(0x4C);                    // Send slave command (0x4C)
@@ -35,30 +13,20 @@ void i2c_read_command(unsigned char address)
 }
 
 void main(void) {
-
-    OSCCON = 0b01101010;                // set internal osc to 8MHz
-    TRISA = 0b000000;                   // set ports to outputs
-    LATA = 0b000000;
-    PORTA = 0b000000;
-
-    i2c_Init();                         // Start I2C as Master 100KHz
-
+    ADCON1 = 0xFF;
+    TRISAbits.TRISA5 = 0;
+    i2c_Init();                         // Configure i2c protocol
+    //__delay_ms(1000);
     while(1)
     {
-        unsigned char read_buff[35];
-        
-        __delay_ms(10);
-        i2c_read_command(I2C_SLAVE);
-        read_buff[0] = i2c_Read(0);            // Read one byte
-        
-        
-        //read_buff[0] = i2c_Read(1);            // Read one byte
-        //read_buff[1] = i2c_Read(1);            // Read one byte
-        //read_buff[2] = i2c_Read(0);            // Read one byte
-        
-        
-        
-        /*
+        char read_buff[35]; // buffer for data reception
+        int tPTAT, tP[16], tPEC; // variables for storing temperature calculations
+                
+        ///////////////////////////////////////////////
+        ///////////// I2C PROTOCOL /////////////
+        i2c_read_command();
+                
+        // return 35 bytes of data from Slave device
         read_buff[0] = i2c_Read(1);
         read_buff[1] = i2c_Read(1);
         read_buff[2] = i2c_Read(1);
@@ -93,11 +61,45 @@ void main(void) {
         read_buff[31] = i2c_Read(1);
         read_buff[32] = i2c_Read(1);
         read_buff[33] = i2c_Read(1);
-        read_buff[34] = i2c_Read(0); //last byte
-        */
-        
+        read_buff[34] = i2c_Read(0); // last byte to be received
+                    
         i2c_Stop();     // send Stop
-    }
+        
+        ///////////////////////////////////////////////
+        ///////////// Compute Temp Values /////////////
+        tPTAT = 256*read_buff[1] + read_buff[0];
+        tP[0] = 256*read_buff[3] + read_buff[2];
+        tP[1] = 256*read_buff[5] + read_buff[4];
+        tP[2] = 256*read_buff[7] + read_buff[6];
+        tP[3] = 256*read_buff[9] + read_buff[8];
+        tP[4] = 256*read_buff[11] + read_buff[10];
+        tP[5] = 256*read_buff[13] + read_buff[12];
+        tP[6] = 256*read_buff[15] + read_buff[14];
+        tP[7] = 256*read_buff[17] + read_buff[16];
+        tP[8] = 256*read_buff[19] + read_buff[18];
+        tP[9] = 256*read_buff[21] + read_buff[20];
+        tP[10] = 256*read_buff[23] + read_buff[22];
+        tP[11] = 256*read_buff[25] + read_buff[24];
+        tP[12] = 256*read_buff[27] + read_buff[26];
+        tP[13] = 256*read_buff[29] + read_buff[28];
+        tP[14] = 256*read_buff[31] + read_buff[30];
+        tP[15] = 256*read_buff[33] + read_buff[32];
+        tPEC = read_buff[34];      
+         
+        ////////////////////////////////////////////////
+        //////////////// Toggle I/O Pin ////////////////
+        
+        RA5 = 1;
+         __delay_ms(100);
+        
+        if(tP[0] >= 0 || tP[6] >= 0) {
+            RA5 = 0;
+            __delay_ms(100);
+        }      
+            
+        __delay_ms(200);
+    }    
+    
     
     
     return; //never reaches this point
--- i2c.h
+++ i2c.h
@@ -1,19 +1,49 @@
+#include <xc.h>
+
+//PIC12F1822 Configuration Bit Settings
+#define I2C_SLAVE 0x0A  // address without R/W bit, bit shifted left to make 14h or 15h
+#define _XTAL_FREQ 4000000 // frequency for __delay_ms
+
+// CONFIG1
+#pragma config FOSC = INTOSC    // Oscillator Selection (INTOSC oscillator: I/O function on CLKIN pin)
+#pragma config WDTE = OFF       // Watchdog Timer Enable (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable (PWRT disabled)
+#pragma config MCLRE = OFF      // MCLR Pin Function Select (MCLR/VPP pin function is digital input)
+#pragma config CP = ON          // Flash Program Memory Code Protection (Program memory code protection is enabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable (Brown-out Reset enabled)
+#pragma config CLKOUTEN = OFF   // Clock Out Enable (CLKOUT function is disabled. I/O or oscillator function on the CLKOUT pin)
+
+// CONFIG2
+#pragma config WRT = OFF        // Flash Memory Self-Write Protection (Write protection off)
+#pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable (Stack Overflow or Underflow will cause a Reset)
+#pragma config BORV = LO        // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (Vbor), low trip point selected.)
+//#pragma config LPBOR = OFF      // Low-Power Brown Out Reset (Low-Power BOR is disabled)
+#pragma config LVP = ON         // Low-Voltage Programming Enable (Low-voltage programming enabled)
+
+
 #define I2C_WRITE 0
 #define I2C_READ 1
 
 // Initialize MSSP - Master Synchronous Serial Port. (12F1822 - other devices may differ)
 void i2c_Init(void){
-
+    //OSCCON = 0b01101010;                // set internal osc to 4MHz
+    //TRISA = 0b000000;                   // set ports to outputs
+    //LATA = 0b000000;
+    //PORTA = 0b000000;
+    
    // Initialize I2C MSSP
-   // SCL needs to be 100KHz or less for OMRON 44L-D6T
+   SSP1CON1 = 0b00101000;   // I2C enabled, Master mode
+   //SSP1CON2bits.SEN = 0;
+   SSP1CON2 = 0x00;
+   SSP1ADD = I2C_SLAVE; // F_clock of SCL = F_osc/(4*(SSP1ADD + 1)) = 90.9kHz < 100kHz
+   SSP1STAT = 0x00;   // Slew rate disabled
+   //SSP1CON3 = 0b11111000;
+   
+   // SCL needs to be 100KHz or less for OMRON 44L-D6T   
    TRISAbits.TRISA1 = 1;    // set SCL and SDA pins as inputs
    TRISAbits.TRISA2 = 1;
-
-   SSP1CON1 = 0b00101000;   // I2C enabled, Master mode
-   SSP1CON2bits.SEN = 0;
-   SSP1ADD = 0x0A;          // 100Khz @ 4Mhz Fosc
-   SSP1STAT = 0b11000000;   // Slew rate disabled
-      
+    
+          
    return;
 }
 
@@ -28,18 +58,24 @@ void i2c_Wait(void)
 // i2c_Start - Start I2C communication
 void i2c_Start(void)
 {
+    i2c_Wait();
+    
+    //while ( ( SSP1CON2 & 0x1F ) || ( SSP1STAT & 0x04 ) ) {
     SSP1CON2bits.SEN = 1;  //Indicates that start bit has been detected last
     //while(SSP1CON2bits.SEN);
-    
-    SSP1CON3 = 0b11101000;
+    //}
+    //SSP1CON3 = 0b11101000;
         
     return;
 }
 
 // i2c_Restart - Re-Start I2C communication
 void i2c_Restart(void){
+    i2c_Wait();
+    
     SSP1CON2bits.RSEN = 1; // Initiates repeated start condition
     //while(SSP1CON2bits.RSEN);
+    //SSP1CON3 = 0b11111000;
     
     return;
 }
@@ -47,6 +83,8 @@ void i2c_Restart(void){
 // i2c_Stop - Stop I2C communication
 void i2c_Stop(void)
 {
+    i2c_Wait();
+    
     SSP1CON2bits.PEN=1;  //Initiate stop condition on SDA and SCL pins, auto cleared by hardware
     //while(SSP1CON2bits.PEN);
     
@@ -56,12 +94,14 @@ void i2c_Stop(void)
 // i2c_Write - Sends one byte of data
 void i2c_Write(unsigned char data)
 {
+    i2c_Wait();
+    
     SSP1BUF = data;  //MSSP1 Receive Buffer/Transmit Register
-    while(SSP1STATbits.BF);       // wait till complete data is sent from buffer 
+    
     //i2c_Wait();
     //SSP1CON2bits.ACKDT = 0;
-    SSP1CON2bits.ACKDT = 0;
-    SSP1CON2bits.ACKEN = 1;                    // send acknowledge sequence
+    //SSP1CON2bits.ACKDT = 0;
+    //SSP1CON2bits.ACKEN = 1;                    // send acknowledge sequence
     //while(SSP1CON2bits.ACKEN);
     
     return;
@@ -71,39 +111,44 @@ void i2c_Write(unsigned char data)
 // mode is either I2C_WRITE or I2C_READ
 void i2c_Address(unsigned char address, unsigned char mode)
 {
+    i2c_Wait();
+    
     unsigned char l_address;
 
     l_address=address<<1;       // Shifts slave address (0x0A)
     l_address+=mode;            // Adds R/W bit to Address to make 14h or 15h
     SSP1BUF = l_address;
-    while(SSP1STATbits.BF);     // wait till complete data is sent from buffer
     
-    SSP1CON2bits.ACKDT = 0;
-    SSP1CON2bits.ACKEN = 1;     // send acknowledge sequence
+    //SSP1CON2bits.ACKDT = 0;    
+    //SSP1CON2bits.ACKEN = 1;     // send acknowledge sequence
+        
     //while(SSP1CON2bits.ACKEN);
     
     return;
 }
 
 // i2c_Read - Reads a byte from Slave device
-unsigned char i2c_Read(unsigned char ack)
+char i2c_Read(unsigned short ack)
 {
     // ack should be 1 if there is going to be more data read
     // ack should be 0 if this is the last byte of data read
     
-    unsigned char i2cReadData;
+    char i2cReadData;
     
     i2c_Wait();
 
     SSP1CON2bits.RCEN=1;
-    while(!(SSP1STATbits.BF));      // wait for receive complete
+    i2c_Wait();
+    
     i2cReadData = SSP1BUF;
-    if ( ack ) {
-        SSP1CON2bits.ACKDT=0;         // Ack
+    i2c_Wait();
+    
+    if(ack == 1) {
+        SSP1CON2bits.ACKDT = 0;         // Ack
     } else {
-        SSP1CON2bits.ACKDT=1;         // NAck
+        SSP1CON2bits.ACKDT = 1;
     }
-    SSP1CON2bits.ACKEN=1;             // send acknowledge sequence
-
-    return( i2cReadData );
+    SSP1CON2bits.ACKEN = 1;             // send acknowledge sequence
+    
+    return i2cReadData;
 }
